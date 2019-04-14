134c79c83417384559f6485da4a7258a0ad7bf52
forax <forax@univ-mlv.fr>
2019-04-14 17:07:38 +0200

    pass --no-server to native-image

--- generate-native-image.sh
+++ generate-native-image.sh
@@ -4,4 +4,5 @@
 echo GRAAL_HOME $GRAAL_HOME
 
 $GRAAL_HOME/bin/native-image \
+  --no-server \
   -cp target/main/artifact/fr.umlv.graalvmdemo-1.3.jar:deps/org.objectweb.asm.jar:deps/org.objectweb.asm.commons.jar:deps/org.objectweb.asm.tree.jar:deps/org.objectweb.asm.tree.analysis.jar fr.umlv.graalvmdemo.Main
