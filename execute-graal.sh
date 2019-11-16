245527eef948c55decc9e968f145782b2c0f646a
forax <forax@localhost>
2019-11-16 17:13:54 +0100

    update script to version 1.4

--- execute-graal.sh
+++ execute-graal.sh
@@ -5,7 +5,7 @@ echo using GRAAL_HOME $GRAAL_HOME
 # classpath version
 $GRAAL_HOME/bin/java \
   -XX:+UseJVMCICompiler \
-  -classpath target/main/artifact/fr.umlv.graalvmdemo-1.3.jar:deps/org.objectweb.asm.jar:deps/org.objectweb.asm.commons.jar \
+  -classpath target/main/artifact/fr.umlv.graalvmdemo-1.4.jar:deps/org.objectweb.asm.jar:deps/org.objectweb.asm.commons.jar \
   fr.umlv.graalvmdemo.Main
 
  
